d204f495d8d4be9ce91fe0f50eff32e9ddac4c42
nondescript <nondescript.que@gmail.com>
2026-01-29 02:42:38 +0600

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,14 @@
-import 'dart:convert';
-
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
-
-Future<bool> fetchLicense() async {
-  const key = "isActiveLicense";
-  final url = Uri.parse(
-    "https://secure.plra.ru/?query=OtQwmiLQxYrNyM72gv6dnLkv4QYrDE",
-  );
-
-  try {
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-
-      final isActive = data['data']['isActive'] as bool;
-
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setBool(key, isActive);
-
-      return isActive;
-    } else {
-
-      final prefs = await SharedPreferences.getInstance();
-      return prefs.getBool(key) ?? true; 
-    }
-  } catch (e) {
-
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(key) ?? true;
-  }
-}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
   await EasyLocalization.ensureInitialized();
-  final isActive = await fetchLicense();
   runApp(
     EasyLocalization(
       supportedLocales: const [
@@ -56,7 +21,7 @@ Future<void> main() async {
       path: 'lang',
       fallbackLocale: const Locale('ru', 'RU'),
       child: ProviderScope(
-        child: isActive ? MyApp(savedThemeMode) : const IsActiveLicense(),
+        child: MyApp(savedThemeMode),
       ),
     ),
   );
